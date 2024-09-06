0df2d9d3965304f64315f13dca7c4f01c69e4fe6
nins-ds <155716399+nins-ds@users.noreply.github.com>
2024-09-06 14:44:02 +0800

    change the url for CORS

--- webapi/webapi/src/main/java/com/grafosofos/webapi/config/WebConfig.java
+++ webapi/webapi/src/main/java/com/grafosofos/webapi/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://grafosofos.netlify.app") // for prod: https://grafosofos.netlify.app for local dev: http://localhost:4200
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);;
