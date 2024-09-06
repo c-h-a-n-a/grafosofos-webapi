a82cc0a1d63cb2e079455419d1f2f3f45726844b
nins-ds <155716399+nins-ds@users.noreply.github.com>
2024-09-06 20:02:05 +0800

    testing this another config class

--- src/main/java/com/grafosofos/webapi/config/WebConfig.java
+++ src/main/java/com/grafosofos/webapi/config/WebConfig.java
@@ -1,14 +1,17 @@
 package com.grafosofos.webapi.config;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig {
 
-//    @Bean
+    //    @Bean
 //    public WebMvcConfigurer corsConfigurer() {
 //        return new WebMvcConfigurer() {
 //            @Override
@@ -21,12 +24,39 @@ public class WebConfig implements WebMvcConfigurer{
 //            }
 //        };
 //    }
-@Override
-public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("https://grafosofos.com")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-}
+//@Override
+//public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//            .allowedOrigins("https://grafosofos.com")
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//            .allowedHeaders("*")
+//            .allowCredentials(true);
+//}
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow credentials if needed
+        config.setAllowCredentials(true);
+
+        // Add your custom domain here
+        config.addAllowedOrigin("https://grafosofos.com");
+
+        // Allow specific methods
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+
+        config.addAllowedMethod("OPTIONS");
+        // Allow headers
+        config.addAllowedHeader("*");
+
+        // Set the exposed headers if needed
+        config.addExposedHeader("Authorization");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter();
+    }
 }
