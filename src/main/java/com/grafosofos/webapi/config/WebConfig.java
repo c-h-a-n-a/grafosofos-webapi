568da60644268db2006b95a0d19b23a5219c6c49
nins-ds <155716399+nins-ds@users.noreply.github.com>
2024-09-16 17:43:38 +0800

    update webconfig to change back to the previous url

--- src/main/java/com/grafosofos/webapi/config/WebConfig.java
+++ src/main/java/com/grafosofos/webapi/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")  // Ensure the correct origin https://grafosofos.com http://localhost:4200
+                .allowedOrigins("https://grafosofos.com")  // Ensure the correct origin https://grafosofos.com http://localhost:4200
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
