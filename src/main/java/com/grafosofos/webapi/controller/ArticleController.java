48198bfbac5b82cfd45f230f18bffc85ce9a00b5
nins-ds <155716399+nins-ds@users.noreply.github.com>
2024-10-16 22:48:31 +0800

    add pagination on article api

--- src/main/java/com/grafosofos/webapi/controller/ArticleController.java
+++ src/main/java/com/grafosofos/webapi/controller/ArticleController.java
@@ -1,9 +1,12 @@
 package com.grafosofos.webapi.controller;
 
 import com.grafosofos.webapi.model.Article;
+import com.grafosofos.webapi.repo.ArticleRepository;
 import com.grafosofos.webapi.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +20,12 @@ public class ArticleController {
 
     private final ArticleService articleService;
 
+    private final ArticleRepository articleRepository;
+
     @Autowired
-    public ArticleController(ArticleService articleService) {
+    public ArticleController(ArticleService articleService, ArticleRepository articleRepository) {
         this.articleService = articleService;
+        this.articleRepository = articleRepository;
     }
 
 
@@ -27,6 +33,13 @@ public class ArticleController {
     public List<Article> getAllArticles() {
         return articleService.getAllArticles();
     }
+
+    @GetMapping("/articles")
+    public ResponseEntity<Page<Article>> getArticles(Pageable pageable) {
+        Page<Article> articles = articleRepository.findAll(pageable);
+        return new ResponseEntity<>(articles, HttpStatus.OK);
+    }
+
 
     @GetMapping("/api/articles/{id}")
     public Article getArticleById(@PathVariable Long id) {
@@ -44,7 +57,7 @@ public class ArticleController {
     public ResponseEntity<Void> handleOptions() {
         log.info("Preflight request received");
         return ResponseEntity.ok()
-                .header("Access-Control-Allow-Origin", "https://grafosofos.com") //http://localhost:4200 https://grafosofos.com
+                .header("Access-Control-Allow-Origin", "http://localhost:4200") //http://localhost:4200 https://grafosofos.com
                 .header("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT,OPTIONS")
                 .header("Access-Control-Allow-Headers", "*")
                 .header("Access-Control-Allow-Credentials", "true")
